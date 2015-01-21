1f1a1be504413e134b3031daee4eef35dd934738
Andy Sipe <ajs.general@gmail.com>
2015-01-21 07:06:08 -0500

    clean up

--- src/Maddir.Core/Engine.cs
+++ src/Maddir.Core/Engine.cs
@@ -1,4 +1,5 @@
 ﻿// Copyright (c) Andy Sipe. All rights reserved. Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
 namespace Maddir.Core {
   public class Engine {
     public void Apply(string root, object layout) {}
--- src/Maddir.UnitTests/Core/EngineTest.cs
+++ src/Maddir.UnitTests/Core/EngineTest.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Andy Sipe. All rights reserved. Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
 using NUnit.Framework;
 
 namespace Maddir.UnitTests.Core {
   [TestFixture]
-  public class EngineTest : BaseTestCase {
-     
-  }
+  public class EngineTest : BaseTestCase {}
 }
