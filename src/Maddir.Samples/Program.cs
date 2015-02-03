f4c7f857f417ea0dcf886ff01072b931cf106fcf
Andy Sipe <ajs.general@gmail.com>
2015-02-03 08:30:03 -0500

    added basic sample exe

---
+++ src/Maddir.Samples/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Maddir.Core.MarkupGeneration;
+using Maddir.Core.TreeGeneration;
+using Snarfz.Core;
+using SupaCharge.Core.IOAbstractions;
+
+namespace Maddir.Samples {
+  internal class Program {
+    private static int Main(string[] args) {
+      try {
+        Execute(args);
+        return 0;
+      } catch (Exception e) {
+        Console.WriteLine(e);
+      }
+      return 1;
+    }
+
+    private static void Execute(params string[] args) {
+      switch (args[0]) {
+        case "browse":
+          Browse();
+          break;
+        case "build":
+          Build();
+          break;
+        default:
+          throw new Exception(string.Format("Unknown command: {0}", args[0]));
+      }
+    }
+
+    private static void Build() {
+      Console.WriteLine();
+      Console.WriteLine("Enter Markup: ");
+      var lines = new List<string>();
+      while (true) {
+        var line = Console.ReadLine();
+        if (line == "")
+          break;
+        lines.Add(line);
+      }
+
+      var markup = string.Join(Environment.NewLine, lines.ToArray());
+      var layout = new Parser().Parse(markup);
+
+      Console.WriteLine();
+      Console.Write("Enter Directory To Build Into: ");
+      var dir = Console.ReadLine();
+
+      new Engine(new DotNetDirectory(), new DotNetFile()).Apply(dir, layout);
+    }
+
+    private static void Browse() {
+      Console.WriteLine();
+      Console.Write("Enter Directory To Browse: ");
+      var dir = Console.ReadLine();
+
+      Console.WriteLine("Browsing {0}", dir);
+      var layout = new DirectoryBrowser(Snarfzer.NewScanner()).Browse(dir);
+      var markup = new MarkupGenerationEngine(new MarkupBuilder()).Apply(layout);
+
+      Console.WriteLine();
+      Console.WriteLine();
+      Console.WriteLine(markup);
+    }
+  }
+}
---
+++ src/Maddir.Samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Maddir.Samples")]
+[assembly: AssemblyDescription("Maddir.Samples")]
+[assembly: AssemblyProduct("Maddir.Samples")]
+[assembly: Guid("35416204-2bc0-43fd-b451-b895a56173a4")]
