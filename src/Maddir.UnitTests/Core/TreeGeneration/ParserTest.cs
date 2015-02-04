a23bd397115723ff7a548335dbab606ff7d67373
Andy Sipe <ajs.general@gmail.com>
2015-02-04 07:42:45 -0500

    added additional parser tests

--- src/Maddir.UnitTests/Core/TreeGeneration/ParserTest.cs
+++ src/Maddir.UnitTests/Core/TreeGeneration/ParserTest.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Andy Sipe. All rights reserved. Licensed under the MIT License (MIT). See License.txt in the project root for license information.
 
+using Maddir.Core;
 using Maddir.Core.Commands;
 using Maddir.Core.Model;
 using Maddir.Core.TreeGeneration;
@@ -92,6 +93,16 @@ namespace Maddir.UnitTests.Core.TreeGeneration {
                                                  new AddFileCommand(2, @"dir0\dir1\file2.txt"),
                                                  new AddDirectoryCommand(2, @"dir0\dir1\dir2"),
                                                  new AddFileCommand(3, @"dir0\dir1\dir2\file3.txt"))));
+    }
+
+    [TestCase("z  file1.txt", "Cannot Parse Markup: 'z  file1.txt'")]
+    [TestCase("f file1.txt", "Cannot Parse Markup: 'f file1.txt'")]
+    [TestCase("f   file1.txt", "Cannot Parse Markup: 'f   file1.txt'")]
+    [TestCase("f", "Cannot Parse Markup: 'f'")]
+    [TestCase("F  file1.txt", "Cannot Parse Markup: 'F  file1.txt'")]
+    public void TestUnmatchedRegexThrowsMaddirException(string line, string expectedMessage) {
+      var ex = Assert.Throws<MaddirException>(() => mParser.Parse(line));
+      Assert.That(ex.Message, Is.EqualTo(expectedMessage));
     }
 
     [SetUp]
