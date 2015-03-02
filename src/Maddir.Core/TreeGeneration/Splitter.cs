be7aa8b12206fc2c3bff7149df22f1788ad8e9be
Andy Sipe <ajs.general@gmail.com>
2015-03-02 07:52:58 -0500

    wip on new markdown parser replacement (not yet integrated)

---
+++ src/Maddir.Core/TreeGeneration/Splitter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Maddir.Core.Model;
+
+namespace Maddir.Core.TreeGeneration {
+  public class Splitter {
+    public Splitter(Settings settings) {
+      var starting = Regex.Escape(settings.ContentStartDelimiter.ToString());
+      var ending = Regex.Escape(settings.ContentEndDelimiter.ToString());
+      mFilePattern = new Regex(@"^(f)(  )+\b([^" + starting + "]+) " + starting + "([^" + ending + "]*)" + ending + "\r?$", RegexOptions.Multiline);
+    }
+
+    public IEntry[] Split(string markup) {
+      markup = markup.Trim();
+
+      return GetMatches(mFilePattern, markup)
+        .Concat(GetMatches(mDirPattern, markup))
+        .OrderBy(match => match.Index)
+        .Select(BuildEntry)
+        .ToArray();
+    }
+
+    private static IEnumerable<Match> GetMatches(Regex regex, string markup) {
+      return regex
+        .Matches(markup)
+        .Cast<Match>();
+    }
+
+    private static IEntry BuildEntry(Match match) {
+      switch (match.Groups[1].Value) {
+        case "f":
+          return new FileEntry(match.Groups[2].Captures.Count - 1,
+                               match.Groups[3].Value.Trim(),
+                               match.Groups[4].Value);
+        case "d":
+          return new DirectoryEntry(match.Groups[2].Captures.Count - 1,
+                                    match.Groups[3].Value.Trim());
+        default:
+          throw new MaddirException("Invalid Type: {0}", match.Groups[1].Value);
+      }
+    }
+
+    private readonly Regex mDirPattern = new Regex(@"^(d)(  )+\b(.+)\r?$", RegexOptions.Compiled | RegexOptions.Multiline);
+    private readonly Regex mFilePattern;
+  }
+}
---
+++ src/Maddir.UnitTests/Core/TreeGeneration/SplitterTest.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using Maddir.Core.Model;
+using Maddir.Core.TreeGeneration;
+using Maddir.Core.Utility;
+using NUnit.Framework;
+
+namespace Maddir.UnitTests.Core.TreeGeneration {
+  [TestFixture]
+  public class SplitterTest : BaseTestCase {
+    public class Validation {
+      public Validation() {
+        ContentStartDelimiter = '[';
+        ContentEndDelimiter = ']';
+      }
+
+      public char ContentEndDelimiter{get;private set;}
+      public char ContentStartDelimiter{get;private set;}
+      public string Markup{get;private set;}
+      public IEntry[] Expected{get;set;}
+
+      public Validation SetMarkup(params string[] lines) {
+        Markup = StringUtils.ToNewLineSepString(lines);
+        return this;
+      }
+
+      public Validation SetExpected(params IEntry[] expected) {
+        Expected = expected;
+        return this;
+      }
+    }
+
+    [TestCaseSource("GetUsageTests")]
+    public void TestUsage(Validation validation) {
+      var splitter = new Splitter(new Settings {
+                                                 ContentStartDelimiter = validation.ContentStartDelimiter,
+                                                 ContentEndDelimiter = validation.ContentEndDelimiter
+                                               });
+      Assert.That(splitter.Split(validation.Markup), Are.EqualTo(validation.Expected));
+    }
+
+    public IEnumerable GetUsageTests() {
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("")
+                                      .SetExpected());
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("    ")
+                                      .SetExpected());
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f  file0.txt []")
+                                      .SetExpected(new FileEntry(0, "file0.txt", "")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("  f  file0.txt []   ")
+                                      .SetExpected(new FileEntry(0, "file0.txt", "")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f  file0.txt [0]")
+                                      .SetExpected(new FileEntry(0, "file0.txt", "0")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f    file0.txt  [a]")
+                                      .SetExpected(new FileEntry(1, "file0.txt", "a")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f      file0.txt    [abc]")
+                                      .SetExpected(new FileEntry(2, "file0.txt", "abc")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("d  dir0")
+                                      .SetExpected(new DirectoryEntry(0, "dir0")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("d    dir0")
+                                      .SetExpected(new DirectoryEntry(1, "dir0")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("d      dir0")
+                                      .SetExpected(new DirectoryEntry(2, "dir0")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f  file0.txt []",
+                                                 "f  file1.txt []")
+                                      .SetExpected(new FileEntry(0, "file0.txt", ""),
+                                                   new FileEntry(0, "file1.txt", "")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f  file0.txt [c1]",
+                                                 "f  file1.txt [c2 c2 c2]",
+                                                 "f  file2.txt [ c3 ]")
+                                      .SetExpected(new FileEntry(0, "file0.txt", "c1"),
+                                                   new FileEntry(0, "file1.txt", "c2 c2 c2"),
+                                                   new FileEntry(0, "file2.txt", " c3 ")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f  file0.txt [c1]",
+                                                 "f    file1.txt [c2 c2 c2]",
+                                                 "f      file2.txt [ c3 ]")
+                                      .SetExpected(new FileEntry(0, "file0.txt", "c1"),
+                                                   new FileEntry(1, "file1.txt", "c2 c2 c2"),
+                                                   new FileEntry(2, "file2.txt", " c3 ")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("d  dir1",
+                                                 "d  dir2",
+                                                 "d  dir3")
+                                      .SetExpected(new DirectoryEntry(0, "dir1"),
+                                                   new DirectoryEntry(0, "dir2"),
+                                                   new DirectoryEntry(0, "dir3")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("d  dir1",
+                                                 "d    dir2",
+                                                 "d      dir3")
+                                      .SetExpected(new DirectoryEntry(0, "dir1"),
+                                                   new DirectoryEntry(1, "dir2"),
+                                                   new DirectoryEntry(2, "dir3")));
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f  file0.txt []",
+                                                 "d  dir0")
+                                      .SetExpected(new FileEntry(0, "file0.txt", ""),
+                                                   new DirectoryEntry(0, "dir0")));
+
+      yield return new TestCaseData(new Validation()
+                                      .SetMarkup("f  file0.txt []",
+                                                 "d  dir0",
+                                                 "f    file1.txt [c1]",
+                                                 "f    file2.txt [c2]",
+                                                 "d    dir1",
+                                                 "d  dir2")
+                                      .SetExpected(new FileEntry(0, "file0.txt", ""),
+                                                   new DirectoryEntry(0, "dir0"),
+                                                   new FileEntry(1, "file1.txt", "c1"),
+                                                   new FileEntry(1, "file2.txt", "c2"),
+                                                   new DirectoryEntry(1, "dir1"),
+                                                   new DirectoryEntry(0, "dir2")));
+    }
+  }
+}
